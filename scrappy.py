ea6f12bb6d4e4544f260c4912d823117b88c1723
jessejamesss <jessejames.ro@gmail.com>
2022-10-19 12:59:50 -0700

    Committing updated code

--- scrappy.py
+++ scrappy.py
@@ -15,19 +15,12 @@ def getClient():
 def getUserInfo(handle):
     client = getClient()
     user = client.get_user(username = handle)
-    return user.data.id
 
-def getTweetReplies(artist_handle, tweetID):
-    repliesArray = []
+    return user
 
-    for reply in tweepy.Cursor(api.search_tweets, q = 'to:{}'.format(artist_handle), since_id = tweetID).items(50):
-        if hasattr(reply, 'in_reply_to_status_id_str'):
-            if reply.in_reply_to_status_id == tweetID:
-                repliesArray.append(reply)
 
-# Tuple of Handles
-testHandles = tuple()
-testHandles = ('jessejames_____', 'arlenelmao')
+# Dictionary of artist name and their Twitter handle
+artistDictionary = {'Jesse James' : 'jessejames_____', 'Arlene' : 'arlenelmao'}
 
 # Authentication
 auth = tweepy.OAuthHandler(config.apiKey, config.apiKeySecret)
@@ -35,20 +28,65 @@ auth.set_access_token(config.accessToken, config.accessTokenSecret)
 
 api = tweepy.API(auth)
 
-# Get Client and User
+# Get Client
 client = getClient()
-# user = getUserInfo()
 
-columns = ['Name', 'Handle', 'Type', 'Tweet Body']
+# Create columns for csv file
+artistColumns = ['Name', 'Handle', 'Username', 'Artist ID', 'Type', 'Tweet ID', 'Tweet Text']
+artistTweetsData = []
+mentionsColumns = ['RTA Name', 'RTA Handle', 'RTA Username', 'RTA ID', 'Handle', 'Username', 'ID', 'Type', 'Tweet ID', 'Tweet Text']
+artistMentionsData = []
 
 # Use pagination to get user tweets
-for handle in testHandles:
-    user = getUserInfo(handle)
-    paginator = tweepy.Paginator(client.get_users_tweets, user, exclude = 'retweets',
-                                 max_results = 100)
+for key in artistDictionary:
+    user = getUserInfo(artistDictionary[key])
+    artistName = key
+    artistHandle = user.data.username
+    artistUsername = user.data.name
+    artistID = user.data.id
+    type = 0
 
-    for page in paginator:
-        #print(page)
+    tweetsPaginator = tweepy.Paginator(client.get_users_tweets, artistID, exclude = 'retweets',
+                                       max_results = 100)
+
+    for page in tweetsPaginator:
         for tweet in page.data:
-            replies = getTweetReplies(handle, tweet.id)
-            print(replies)
+            tweetID = tweet.id
+            tweetText = tweet.text
+
+            artistTweetsData.append([artistName, artistHandle, artistUsername, artistID, type, tweetID, tweetText])
+
+for key in artistDictionary:
+    user = getUserInfo(artistDictionary[key])
+    artistName = key
+    artistHandle = user.data.username
+    artistUsername = user.data.name
+    artistID = user.data.id
+    type = 1
+    
+    mentionsPaginator = tweepy.Paginator(client.get_users_mentions, artistID, 
+                                         max_results = 100)
+
+    for page in mentionsPaginator:
+        for mention in page.data:
+            mentionID = mention.id
+            mentionText = mention.text
+
+            mentionData = client.get_tweet(511651662461952001, expansions='author_id', 
+                                           user_fields=['id','name', 'username'])
+
+            mentions = mentionData.includes['users']
+
+            mentionUsername = mentionData.includes['users'].name
+            mentionHandle = mentionData.includes['users'].username
+            mentionUserID = mentionData.includes['users'].id
+
+            artistMentionsData.append([artistName, artistHandle, artistUsername, artistID, mentionHandle, mentionUsername, mentionUserID, type, mentionID, mentionText])
+
+artist_df = pd.DataFrame(artistTweetsData, columns=artistColumns)
+mentions_df = pd.DataFrame(artistMentionsData, columns=mentionsColumns)
+
+artist_df.to_csv('artist_tweets.csv')
+mentions_df.to_csv('artist_mentions.csv')
+
+
